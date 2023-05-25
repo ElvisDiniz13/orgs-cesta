be383f845162def03ba2edbf734af21dea9611f6
ElvisDiniz13 <elvisdiniz13@hotmail.com>
2023-05-25 11:07:04 -0300

    finalizacao

--- App.js
+++ App.js
@@ -1,33 +1,26 @@
-import { StatusBar, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 import { 
   useFonts, 
   Montserrat_400Regular, 
-  Montserrat_700Bold
+  Montserrat_700Bold,
 } from '@expo-google-fonts/montserrat';
-
-import * as SplashScreen from 'expo-splash-screen';
-
 import Cesta from './src/telas/Cesta';
-import mock from './src/mocks/cesta'
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+import mock from './src/mocks/cesta';
 
 export default function App() {
-  let [fonteCarregada] = useFonts({
+  const [fonteCarregada] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold,
   });
 
-  if (!fonteCarregada){
-    return <View/>
+  if (!fonteCarregada) {
+    return <AView/>
   }
 
   return (
-    <SafeAreaView>
-      <StatusBar/>
-      <Cesta {...mock}/>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
-
